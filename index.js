ea43ddceae6db5cf45bafb2e676a894a04e38688
VivekDeyVD <vivekvd35@gmail.com>
2024-08-16 21:23:12 +0530

    new file added

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("page3-leftbox").addEventListener("click",function(){
+    const origiinalContent=this.innerHTML;
+    this.classList.toggle("zoomed-out");
+    
+    // this.innerHTML=''
+    // this.classList.toggle("page3-leftbox");
+    // const newParagraph=document.createElement("p");
+    // newParagraph.innerHTML='Creativity meets new efficiency.Our solutions cuts costs and boosts perforamance.';
+    // this.appendChild(newParagraph);
+
+   
+});
+
